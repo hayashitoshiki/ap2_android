1546afa625256a541261c5bf4d1c4e746abf5950
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2017-09-18 22:44:03 +0900

    GoogleMap修正(詳細、マーカー追加)

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
@@ -10,13 +10,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-
+import com.google.android.gms.maps.model.Marker;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private Marker miti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +27,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-                          /*結合後緯度経度はデータベースからとってくる  */
-        LatLng tokyo = new LatLng(35.605802, 139.735325);       //結合後データベースから店舗名持ってくる
+                          /*検索結果から緯度経度取得  */
+        LatLng tokyo = new LatLng(35.605802, 139.735325);       //検索結果から店舗名取得　store_name
         mMap.addMarker(new MarkerOptions().position(tokyo).title("ヤマダ電機 LABI品川大井町店"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo));
 
+        //現在位置情報
+        //GPS取得　　　　　　　検索結果からGPS情報取得
+        LatLng iti = new LatLng(35.616815, 139.7454031);
+        miti = mMap.addMarker(new MarkerOptions().position(iti).title("現在地"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(iti));
+        miti.setTag(0);
+
+        //   中心の設定                                                //検索結果よりGPS情報の取得
         CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom( new LatLng(35.605, 139.735), 14);
         mMap.moveCamera(cUpdate);
     }
