a20c867b7c49884c13aa4734e0c288814accfa51
sakaihirotaka <s1521077@cco.kanagawa-it.ac.jp>
2017-09-19 20:10:38 +0900

    ホーム画面途中　Javaムズイ

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MainActivity.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MainActivity.java
@@ -2,15 +2,37 @@ package com.example.a1521093.ap2_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText;
+    Button button;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Log.d("", "onCreate()");
 
+        textView = (TextView) findViewById(R.id.textview);
+
+        editText = (EditText) findViewById(R.id.edittext);
+
+        button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+                // 取得したテキストを 検索結果に送る
+                textView.setText(text);
+            }
+        });
+    }
 
 }
