6b26de1f770ccfd9b5e19b31584993521716e667
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2017-09-26 17:04:46 +0900

    GPSをmapに反映

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
@@ -33,18 +33,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
                           /*検索結果から緯度経度取得  */
-        LatLng tokyo = new LatLng(35.605802, 139.735325);       //検索結果から店舗名取得　store_name
-        mMap.addMarker(new MarkerOptions().position(tokyo).title("ヤマダ電機 LABI品川大井町店"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(tokyo));
+        LatLng store_iti = new LatLng(35.605802, 139.735325);       //検索結果から店舗名取得　store_name
+        mMap.addMarker(new MarkerOptions().position(store_iti).title("ヤマダ電機 LABI品川大井町店"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(store_iti));
 
         //現在位置情報
         Intent in = getIntent();
         double latitude1 = in.getDoubleExtra("lat",0);
         double longitude1= in.getDoubleExtra("lon",0);
         //GPS取得　　　　　　　検索結果からGPS情報取得
-        LatLng iti = new LatLng(latitude1, longitude1);
-        miti = mMap.addMarker(new MarkerOptions().position(iti).title("現在地"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(iti));
+        LatLng user_iti = new LatLng(latitude1, longitude1);
+        miti = mMap.addMarker(new MarkerOptions().position(user_iti).title("現在地"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(user_iti));
         miti.setTag(0);
 
         //   中心の設定                                                //検索結果よりGPS情報の取得
