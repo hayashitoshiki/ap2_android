07964cfd3c4d1b99b1480b596c3d37aeeaef2575
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2017-09-26 18:10:31 +0900

    検索等、細かな微調整

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/kategori.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/kategori.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 
@@ -25,16 +26,19 @@ public class kategori extends  AppCompatActivity {
             View view = getLayoutInflater().inflate(R.layout.kategori_sub, null);
             layout.addView(view);
             TextView text = (TextView) view.findViewById(R.id.sub);
-            text.setText( i+1+"こ目のデータ");
+            text.setText( i+1+"こ目のメーカー名");
 
             Button btn = (Button)findViewById(R.id.susumu);
             btn.setId(i);
 
             btn.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    Intent intent = new Intent(getApplication(),kennsakukekaActivity.class);
-                    String da="商品名A";
-                    intent.putExtra("syohin",da);
+                    Intent intent = new Intent(getApplication(),maker.class);
+                    String maker_dai="メーカー名";
+                    intent.putExtra("maker",maker_dai);
+                    Intent kate_dai = getIntent();
+                    String kate = kate_dai.getStringExtra("dai");
+                    intent.putExtra("dai",kate);
                     startActivity(intent);
                 }
             });
@@ -49,6 +53,18 @@ public class kategori extends  AppCompatActivity {
 
             }
         });
+        Button ken=(Button)findViewById(R.id.kensaku);
+        ken.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                EditText SendValue = (EditText)findViewById(R.id.kensakutext);
+                String syohin = SendValue.getText().toString();
+                Intent intent=new Intent(getApplication(),GPS.class);
+                intent.putExtra("syohin",syohin);
+                startActivity(intent);
 
+            }
+        });
     }
+
 }
--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/maker.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/maker.java
@@ -1,9 +1,71 @@
 package com.example.a1521093.ap2_android;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class maker extends AppCompatActivity {
+    @Override
 
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maker);
 
+        TextView dai = (TextView)findViewById(R.id.kensakugamen);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("maker");
+        dai.setText(data);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.test);
+        for(int i=0;i<8;i++) {
+            View view = getLayoutInflater().inflate(R.layout.kategori_sub, null);
+            layout.addView(view);
+            TextView text = (TextView) view.findViewById(R.id.sub);
+            text.setText( i+1+"こ目の商品名");
+
+            Button btn = (Button)findViewById(R.id.susumu);
+
+            btn.setId(i);
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplication(),GPS.class);
+                    String da="商品名A";
+                    intent.putExtra("syohin",da);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        Button sendButton=(Button)findViewById(R.id.modoru);
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent intent=new Intent(getApplication(),kategori.class);
+                Intent kate_dai = getIntent();
+                String kate = kate_dai.getStringExtra("dai");
+                intent.putExtra("dai",kate);
+                startActivity(intent);
+
+            }
+        });
+        Button ken=(Button)findViewById(R.id.kensaku);
+        ken.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                EditText SendValue = (EditText)findViewById(R.id.kensakutext);
+                String syohin = SendValue.getText().toString();
+                Intent intent=new Intent(getApplication(),GPS.class);
+                intent.putExtra("syohin",syohin);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
