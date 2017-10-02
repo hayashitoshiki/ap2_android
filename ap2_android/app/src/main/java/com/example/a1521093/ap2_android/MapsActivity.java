30b35a56ef91a9cad1238cf22c8dcd38441e0060
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2017-10-03 00:44:54 +0900

    ルート表示機能追加(別タブ、マップ)

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.a1521093.ap2_android;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -70,5 +71,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //   中心の設定
         CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom( new LatLng(user_latitude, user_longitude), 14);
         mMap.moveCamera(cUpdate);
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
+        intent.setData(Uri.parse("http://maps.google.com/maps?saddr="+user_latitude+","+user_longitude+"&daddr="+store_latitude+","+store_longitude));
+        startActivity(intent);
+
     }
 }
