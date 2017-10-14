ecb22c6fe4e4753e3b7d96c723256eab40aa218d
hayashitoshiki <s1521093@cco.kanagawa-it.ac.jp>
2017-10-14 17:49:19 +0900

    商品一覧画面追加

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MainActivity.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public  void onClick(View v){
                 Intent intent=new Intent(getApplication(),kategori.class);
-                dai="カテゴリ：化粧品";
+                dai="化粧品";
                 intent.putExtra("kategori",dai);
                 startActivity(intent);
 
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public  void onClick(View v){
                 Intent intent=new Intent(getApplication(),kategori.class);
-                dai="カテゴリ：家電製品";
+                dai="家電製品";
                 intent.putExtra("kategori",dai);
                 startActivity(intent);
 
