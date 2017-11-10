5166bf34c9fc3ec18681ad8a68d2284bf7b554e5
sakaihirotaka <s1521077@cco.kanagawa-it.ac.jp>
2017-11-10 22:33:42 +0900

    アカウント作成画面　Java追加　ホームの下タブから画面遷移　サーバの実装がまだなので通信関係は未実装

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/newacount.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/newacount.java
@@ -12,16 +12,28 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class newacount extends AppCompatActivity {
 
+    private ApiService ApiService;
+    private Button button2 =null;
+    private EditText editText7 = null;
+    private EditText editText8 = null;
+    private EditText editText9 = null;
     String dai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newacount);
 
-        Button kesyo=(Button)findViewById(R.id.modoru);
-        kesyo.setOnClickListener(new View.OnClickListener(){
+        //戻るボタン
+        Button modorou=(Button)findViewById(R.id.modoru);
+        modorou.setOnClickListener(new View.OnClickListener(){
             @Override
 
             public  void onClick(View v){
@@ -31,7 +43,28 @@ public class newacount extends AppCompatActivity {
             }
         });
 
-        
+        Button sousin=(Button)findViewById(R.id.button2);
+        sousin.setOnClickListener(new View.OnClickListener(){
+            @Override
 
+            //パスワード比較
+            public  void onClick(View v){
+                EditText  a1 = (EditText)findViewById(R.id.editText9);
+                EditText  a2 = (EditText)findViewById(R.id.editText10);
+                String b1 = a1.getText().toString();
+                String b2 = a2.getText().toString();
+
+                if(b1.equals(b2) == false){
+                    //不一致の場合の処理
+                    Intent intent=new Intent(getApplication(),newacount.class);
+                    intent.putExtra("dai",dai);
+                    startActivity(intent);
+                }
+            }
+
+
+        });
     }
 }
+
+
