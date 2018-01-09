a273ad137260bae897c0b555c159538cbf9c722d
sakaihirotaka <s1521077@cco.kanagawa-it.ac.jp>
2018-01-09 14:21:52 +0900

    画面完成　機能はまだ　ホームから遷移 ホームとアカウントに遷移可

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/Favarite.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/Favarite.java
@@ -4,21 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Favarite extends AppCompatActivity {
+    String dai;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favarite);
+
+        ImageButton acounts2 = (ImageButton) findViewById(R.id.account_button2);
+        acounts2.setOnClickListener(new View.OnClickListener() {
+            /** ボタンをクリックした時に呼ばれる */
+            @Override
+            public void onClick(View v) {
+                // ここに処理を記述する
+                Intent intent=new Intent(getApplication(),Account.class);
+                intent.putExtra("dai",dai);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton home2 = (ImageButton) findViewById(R.id.homebutton2);
+        home2.setOnClickListener(new View.OnClickListener() {
+            /** ボタンをクリックした時に呼ばれる */
+            @Override
+            public void onClick(View v) {
+                // ここに処理を記述する
+                Intent intent=new Intent(getApplication(),MainActivity.class);
+                intent.putExtra("dai",dai);
+                startActivity(intent);
+            }
+        });
     }
 
 
-    public void homebutton2(View v){
-        Intent i = new Intent(this,MainActivity.class);
-        startActivity(i);
-    }
-
-    public void account_button2(View v){
-        Intent i = new Intent(this,Account.class);
-        startActivity(i);
-    }
 }
