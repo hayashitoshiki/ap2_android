175e257e313141b23a96e7f3dc8ac16e4c22e461
sakaihirotaka <s1521077@cco.kanagawa-it.ac.jp>
2018-01-09 17:20:40 +0900

    Merge remote-tracking branch 'origin/master' into sakai
    # Conflicts:
    #	ap2_android/app/src/main/AndroidManifest.xml
    #	ap2_android/app/src/main/java/com/example/a1521093/ap2_android/Account.java
    #	ap2_android/app/src/main/java/com/example/a1521093/ap2_android/MainActivity.java
    #	ap2_android/app/src/main/res/layout/activity_main.xml

--- ap2_android/app/src/main/java/com/example/a1521093/ap2_android/Favorite.java
+++ ap2_android/app/src/main/java/com/example/a1521093/ap2_android/Favorite.java
@@ -4,25 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class Favorite extends AppCompatActivity {
+    String dai;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.favorite);
+        setContentView(R.layout.favarite);
+
+        ImageButton acounts2 = (ImageButton) findViewById(R.id.account_button2);
+        acounts2.setOnClickListener(new View.OnClickListener() {
+            /** ボタンをクリックした時に呼ばれる */
+            @Override
+            public void onClick(View v) {
+                // ここに処理を記述する
+                Intent intent=new Intent(getApplication(),Account.class);
+                intent.putExtra("dai",dai);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton home2 = (ImageButton) findViewById(R.id.homebutton2);
+        home2.setOnClickListener(new View.OnClickListener() {
+            /** ボタンをクリックした時に呼ばれる */
+            @Override
+            public void onClick(View v) {
+                // ここに処理を記述する
+                Intent intent=new Intent(getApplication(),MainActivity.class);
+                intent.putExtra("dai",dai);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void Logout(View v){
-        Intent intent=new Intent(getApplication(),AccountLogin.class);
-        startActivity(intent);
-    }
 
-    public void home_button(View v){
-        Intent intent = new Intent(getApplication(),MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void account(View v){
-        Intent intent = new Intent(getApplication(),Account.class);
-        startActivity(intent);
-    }
 }
